9150fffdba457c3f08a027fbc19eec2054abe9f3
Vladislav Flinski <flinssski@gmail.com>
2025-08-04 19:49:01 +0300

    feat: add gallery layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Header'
+import Carousel from '@/components/Carousel'
 import styles from './App.module.scss'
 
 export default function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <Carousel />
     </div>
   )
 }
---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,17 @@
+import styles from './Carousel.module.scss'
+
+export default function Carousel() {
+  return (
+    <div className={styles.carousel}>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
